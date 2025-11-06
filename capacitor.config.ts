d2fea8742c733b47ab6bef7788854b52c543a824
Davi10x <davicoox10@gmail.com>
2025-11-06 09:31:55 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pokeDavid',
+  webDir: 'www'
+};
+
+export default config;
